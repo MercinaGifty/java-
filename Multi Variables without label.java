199d51d401ddbc7cdc029ddc4fb905a6c3c34308
Mercy <mercysam232007@gmail.com>
2026-08-10 18:45:46 +0530

    Create Multi Variables without label.java

---
+++ Multi Variables without label.java
@@ -0,0 +1,16 @@
+public class MultiVariablesWithoutLabel {
+    public static void main(String[] args) {
+        String name="Mercy";//String variable
+        int age=19;//integer variable
+        char grade='A';//character variable
+        double percentage=96.5;//double variable
+        boolean ismale=false;//boolean variable
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(percentage);
+        System.out.println(ismale);
+    }
+}
+
+

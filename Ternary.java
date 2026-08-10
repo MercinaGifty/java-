04f28102da85299966d79251f9947238d05f4026
Mercy <mercysam232007@gmail.com>
2026-08-10 23:26:09 +0530

    Create Ternary.java

---
+++ Ternary.java
@@ -0,0 +1,7 @@
+public class Ternary {
+    public static void main(string[]args){
+        int age=18;
+        String result = (age>=18)?"Eligible to vote":"Not Eligible to vote";
+        System.out.println(result);
+    }
+}

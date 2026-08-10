a8a3f7f9aa1a8c2bbb72ac3b1ee93bc50407fbea
Mercy <mercysam232007@gmail.com>
2026-08-10 21:28:39 +0530

    Create Assignment operator.java

---
+++ Assignment operator.java
@@ -0,0 +1,15 @@
+package Operators;
+public class Assignment {
+    public static void main(String[]args){
+        int a=10;
+        System.out.println("Initial Value: "+a);
+        a+=5;//a=a+5
+        System.out.println("After+=: "+a);
+        a-=5;//a=a-5        
+        System.out.println("After-=: "+a);
+        a*=5;//a=a*5
+        System.out.println("After*=: "+a);
+        a/=5;//a=a/5
+        System.out.println("After/=: "+a);
+    }
+}

c286fdd7734c9561c092a56adff87eca6461a85f
Mercy <mercysam232007@gmail.com>
2026-08-10 22:10:27 +0530

    Create Relational Operator.java

---
+++ Relational Operator.java
@@ -0,0 +1,14 @@
+package Operators;
+
+public class Relational{
+    public static void main(String[] args) {
+
+        int x = 15;
+        int y = 10;
+
+        System.out.println("x < y : " + (x < y));
+        System.out.println("x > y : " + (x > y));
+        System.out.println("x <= y : " + (x <= y));
+        System.out.println("x >= y : " + (x >= y));
+    }
+}

796571ba505379e6a448c86cdb78ee6e86ed8caf
Mercy <mercysam232007@gmail.com>
2026-08-10 18:38:38 +0530

    Create print and println difference.java

---
+++ print and println difference.java
@@ -0,0 +1,12 @@
+public class printAndprintln {
+    public static void main(String[]args){
+        System.out.print("God is good");
+        System.out.println("All the time god is good");
+    
+    
+        System.out.println("God is good");
+        System.out.print("All the time god is good");
+    
+    }
+
+}

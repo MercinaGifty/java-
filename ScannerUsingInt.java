7a7bfef692dafdd6fd07ebbcc57d17dcaaf879f3
Mercy <mercysam232007@gmail.com>
2026-08-10 23:31:30 +0530

    Create ScannerUsingInt.java

---
+++ ScannerUsingInt.java
@@ -0,0 +1,11 @@
+public class ScannerUsingInt {
+public static void main(String[]args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter age: ");
+    int age=sc.nextInt();
+    System.out.println("Age: "+age);
+    sc.close();
+}
+}
+
+}

2de8fbbfb3dc82453aeca8925feb778d5dc6939e
Mercy <mercysam232007@gmail.com>
2026-08-10 18:49:04 +0530

    Create Variables with label.java

---
+++ Variables with label.java
@@ -0,0 +1,15 @@
+public class variablewithlabel {
+    public static void main(String[] args) {
+        String name="Mercy";
+        int age=19;
+        char grade='A';
+        double percentage=96.5;
+        boolean ismale=false;
+
+        System.out.println("Name: "+name);
+        System.out.println("Age: "+age);
+        System.out.println("Grade: "+grade);
+        System.out.println("Percentage: "+percentage);
+        System.out.println("You're a Male: "+ismale);
+    }
+}

7c80a02f433e73f029611b6dbcb407f2ba824a3c
Mercy <mercysam232007@gmail.com>
2026-08-10 18:51:43 +0530

    Add ChangeVariableValue class to demonstrate variable change

---
+++ Change Variable Value.java
@@ -0,0 +1,8 @@
+public class ChangeVariableValue {
+    public static void main(String[] args) {
+        int x = 10;
+        System.out.println(x);
+        x = 20;
+        System.out.println(x);
+    }
+}

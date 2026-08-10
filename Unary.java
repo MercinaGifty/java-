67336a2639dfe4713c26abf28414b6566f176bf0
Mercy <mercysam232007@gmail.com>
2026-08-10 23:12:12 +0530

    Create Unary.java

---
+++ Unary.java
@@ -0,0 +1,16 @@
+public class Unary {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        boolean isNumber = true;
+
+        System.out.println(+a);
+        System.out.println(-b);
+
+        System.out.println(++a);
+        System.out.println(--b);
+
+        System.out.println(!isNumber);
+    }
+}

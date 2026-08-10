2e790853a0f3afc0dd6f9df9d46f80f964ccea66
Mercy <mercysam232007@gmail.com>
2026-08-10 19:05:38 +0530

    Create Data Type.java

---
+++ Data Type.java
@@ -0,0 +1,22 @@
+public class DataType {
+    public static void main(String[] args) {
+        byte age = 19;//byte variable
+        short salary = 30000;//short variable
+        int marks = 95;//int variable
+        long population = 1400000000L;//long variable
+        float price = 99.5f;//float variable
+        double pi = 3.14159;//double variable
+        char grade = 'A'; //character variable
+        boolean passed = true;//boolean variable
+
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(marks);
+        System.out.println(population);
+        System.out.println(price);
+        System.out.println(pi);
+        System.out.println(grade);
+        System.out.println(passed);
+    }
+}
+

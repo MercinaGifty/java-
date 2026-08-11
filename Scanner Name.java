95687f6de770d244628cea334a2c62699c95fac1
Mercy <mercysam232007@gmail.com>
2026-08-11 20:35:42 +0530

    Remove sc.close() from Scanner Name.java
    Remove scanner close method to prevent resource closing.

---
+++ Scanner Name.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Name {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your name: ");
+        String name = sc.next();
+
+        System.out.println("Your name is: " + name);
+
+    }
+}

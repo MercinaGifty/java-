0acfdc351e5bd04a49a3bd9552a5a06a0f72082e
Mercy <mercysam232007@gmail.com>
2026-08-12 00:00:35 +0530

    Add Explicit class for type casting example

---
+++ Explicit.java
@@ -0,0 +1,11 @@
+package TypeCasting;
+
+public class Explicit {
+    public static void main(String[]args){
+        int a=10;
+        double b=(double)a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
+

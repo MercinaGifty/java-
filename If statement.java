eccb447adec68499130af84f2ca198e362e688b7
Mercy <mercysam232007@gmail.com>
2026-08-12 10:09:05 +0530

    Create If statement.java

---
+++ If statement.java
@@ -0,0 +1,10 @@
+package DecisionMaking;
+
+public class ifStatement {
+    public static void main(String[]args){
+        int age=20;
+        if(age>=18){
+            System.out.println("Eligible to vote");
+        }
+    }
+}

56fd17dcde28ac21fd4e1c6e3b5e1770df2fac97
Mercy <mercysam232007@gmail.com>
2026-08-13 21:53:56 +0530

    Create elseifStatement.java

---
+++ elseifStatement.java
@@ -0,0 +1,26 @@
+package DecisionMaking;
+
+public class elseifStatement {
+    public static void main(String[] args) {
+        int mark=65;
+        if(mark>=90){
+            System.out.println("Grade A");
+        }
+        else if(mark>=80){
+            System.out.println("Grade b");
+        }
+        else if(mark>=70){
+            System.out.println("Grade c");
+        }
+        else if(mark>=60){
+            System.out.println("Grade d");
+        }
+        else{
+            System.out.println("Fail");
+        }
+    }
+}
+
+    
+
+
